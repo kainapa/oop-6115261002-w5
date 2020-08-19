4d076c18ca5d980ce47f82f668938b29105fe695
kainapa <68212816+kainapa@users.noreply.github.com>
2020-08-19 17:58:04 +0700

    Add files via upload

---
+++ Building.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oop_6115261002_w5
+{
+    class Building
+    {
+        private string buildingCode;
+        private string buildingName;
+        private string buildingLat;
+        private string buildingLong;
+        public string BuildingCode
+        {
+            get { return buildingCode; }
+            set { buildingCode = value; }
+        }
+        public string BuildingName
+        {
+            get { return buildingName; }
+            set { buildingName = value; }
+        }
+        public string BuildingLat
+        {
+            get { return buildingLat; }
+            set { buildingLat = value; }
+        }
+        public string BuildingLong
+        {
+            get { return buildingLong; }
+            set { buildingLong = value; }
+        }
+
+        public Building() { }
+        public Building(string bc, string bn, string blat, string blong)
+        {
+            this.BuildingCode = bc;
+            this.BuildingName = bn;
+            this.BuildingLat = blat;
+            this.BuildingLong = blong;
+        }
+
+        public override string ToString()
+        {
+            return this.BuildingName + " ";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace oop_6115261002_w5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                Building dgka = new Building("5", "computer", "659", "287");
+                Room jrhr = new Room("555", "952", "4", "4", "computer", dgka);
+                Subject sfy = new Subject("002", "programming", "3", "3", "5");
+                Lecturer fti = new Lecturer("002", "jag", "iyf", "professor");
+                Section yvsf = new Section("02", "Monday", "8.00AM", "12.00PM", jrhr, sfy, fti);
+                Console.WriteLine(yvsf);
+        }
+    }
+}
